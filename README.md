1dfb2e41f0cdf89fe44e9d47a313f99256f1d793
Ibrahiem <simplex3@campus.technion.ac.il>
2017-09-25 21:37:54 +0300

    Added future plans

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Fetch total vacancies for a list of courses online (in the university's website - using web scraping).
 
 Quick check or busywait for the course to be vacant and beeps when a spot is available.
+
+
+
+Future Plans:
+Find the optimal TTL
+Find the optimal delay - catch error 529
+Check if refreshing the page online gives the same error
--- configs.py
+++ configs.py
@@ -8,8 +8,5 @@ COURSE_NUMBERS = {'234118'}  # {324580}
 REFRESH_RATE = 5  # seconds {5}
 STOP_ON_SUCCESS = False  # {True}
 BEEP_ON_SUCCESS = False  # {True}
-TTL = 45  # Time to live (repititions) {255}
-#	TODO: Run to find the optimal TTL
-# MAX_TLL: 45 on a 5 REFRESH_RATE
-#	TODO: Run to find the optimal delay - catch error 529
-#	Refreshing the page gives the same error!
+TTL = 45  # Time to live (repititions) {45}
+# ERROR 529 STATISTICS (TTL/RR): 45/5
